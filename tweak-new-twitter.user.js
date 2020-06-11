824fd51933d757c31b541a246d11523113205887
Jonny Buchanan <jonathan.buchanan@gmail.com>
2020-06-12 02:18:51 +1000

    Release v1.19
    - Fix the divider above headings not being hidden
    - Fix retweets being hidden when configured to ignore them

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     19
+// @version     20
 // ==/UserScript==
 
 //#region Config & variables
