3363e3104c46e21e54f0f0aff3416619eea90e8f
Jonny Buchanan <jonathan.buchanan@gmail.com>
2021-07-21 18:14:52 +1000

    Release v2.0
    Closes #47

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     28
+// @version     2.0
 // ==/UserScript==
 
 const enableDebugLogging = false
