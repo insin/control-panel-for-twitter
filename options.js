bb668bf12488bbf58e3b88d199ec94010bdfc7c8
Jonny Buchanan <jonathan.buchanan@gmail.com>
2021-07-28 13:08:12 +1000

    Only store options changed by the user, so changes to defaults for new features will be picked up as long as the user hasn't set their own preference
    Closes #50

--- options.js
+++ options.js
@@ -40,70 +40,84 @@ const checkboxGroups = new Map(Object.entries({
   ].filter(Boolean),
 }))
 
-chrome.storage.local.get((storedConfig) => {
-  let $form = document.querySelector('form')
+/**
+ * @type {import("./types").Config}
+ */
+const defaultConfig = {
+  // Shared
+  addAddMutedWordMenuItem: true,
+  alwaysUseLatestTweets: true,
+  fastBlock: true,
+  hideAnalyticsNav: true,
+  hideBookmarksNav: true,
+  hideListsNav: true,
+  hideMomentsNav: true,
+  hideMoreTweets: true,
+  hideNewslettersNav: true,
+  hideShareTweetButton: false,
+  hideTopicsNav: true,
+  hideTweetAnalyticsLinks: false,
+  hideTwitterAdsNav: true,
+  hideWhoToFollowEtc: true,
+  likedTweets: 'hide',
+  quoteTweets: 'ignore',
+  repliedToTweets: 'hide',
+  retweets: 'separate',
+  tweakQuoteTweetsPage: true,
+  // Experiments
+  hideMetrics: false,
+  reducedInteractionMode: false,
+  verifiedAccounts: 'ignore',
+  // Desktop only
+  hideAccountSwitcher: true,
+  hideExploreNav: true,
+  hideMessagesDrawer: true,
+  hideSidebarContent: true,
+  navBaseFontSize: true,
+  // Mobile only
+  hideAppNags: true,
+  hideExplorePageContents: true,
+  hideMessagesBottomNavItem: false,
+}
 
-  /**
-   * @type {import("./types").Config}
-   */
-  let config = {
-    // Shared
-    addAddMutedWordMenuItem: true,
-    alwaysUseLatestTweets: true,
-    fastBlock: true,
-    hideAnalyticsNav: true,
-    hideBookmarksNav: true,
-    hideListsNav: true,
-    hideMomentsNav: true,
-    hideMoreTweets: true,
-    hideNewslettersNav: true,
-    hideShareTweetButton: false,
-    hideTopicsNav: true,
-    hideTweetAnalyticsLinks: false,
-    hideTwitterAdsNav: true,
-    hideWhoToFollowEtc: true,
-    likedTweets: 'hide',
-    quoteTweets: 'ignore',
-    repliedToTweets: 'hide',
-    retweets: 'separate',
-    tweakQuoteTweetsPage: true,
-    // Experiments
-    hideMetrics: false,
-    reducedInteractionMode: false,
-    verifiedAccounts: 'ignore',
-    // Desktop only
-    hideAccountSwitcher: true,
-    hideExploreNav: true,
-    hideMessagesDrawer: true,
-    hideSidebarContent: true,
-    navBaseFontSize: true,
-    // Mobile only
-    hideAppNags: true,
-    hideExplorePageContents: true,
-    hideMessagesBottomNavItem: false,
-    ...storedConfig
-  }
+/**
+ * Complete configuration for the options page.
+ * @type {import("./types").Config}
+ */
+let optionsConfig
+
+/**
+ * Configuration changes made by the user.
+ * @type {Partial<import("./types").Config>}
+ */
+let userConfig
+
+chrome.storage.local.get((storedConfig) => {
+  userConfig = storedConfig
+  optionsConfig = {...defaultConfig, ...userConfig}
+
+  let $form = document.querySelector('form')
 
   function updateCheckboxGroups() {
     for (let [group, checkboxNames] of checkboxGroups.entries()) {
-      let checkedCount = checkboxNames.filter(name => config[name]).length
+      let checkedCount = checkboxNames.filter(name => optionsConfig[name]).length
       $form.elements[group].checked = checkedCount == checkboxNames.length
       $form.elements[group].indeterminate = checkedCount > 0 && checkedCount < checkboxNames.length;
     }
   }
 
-  for (let prop in config) {
+  for (let prop in optionsConfig) {
     if (prop in $form.elements) {
       if ($form.elements[prop].type == 'checkbox') {
-        $form.elements[prop].checked = config[prop]
+        $form.elements[prop].checked = optionsConfig[prop]
       }
       else {
-        $form.elements[prop].value = config[prop]
+        $form.elements[prop].value = optionsConfig[prop]
       }
     }
   }
 
-  document.body.classList.toggle('home', !config.alwaysUseLatestTweets)
+  document.body.classList.toggle('home', !optionsConfig.alwaysUseLatestTweets)
 
   updateCheckboxGroups()
 
@@ -112,18 +126,18 @@ chrome.storage.local.get((storedConfig) => {
     if ($el.type == 'checkbox') {
       if (checkboxGroups.has($el.name)) {
         checkboxGroups.get($el.name).forEach(checkboxName => {
-          config[checkboxName] = $form.elements[checkboxName].checked = $el.checked
+          optionsConfig[checkboxName] = userConfig[checkboxName] = $form.elements[checkboxName].checked = $el.checked
         })
         $el.indeterminate = false
       } else {
-        config[$el.name] = $el.checked
+        optionsConfig[$el.name] = userConfig[$el.name] = $el.checked
         updateCheckboxGroups()
       }
     }
     else {
-      config[$el.name] = $el.value
+      optionsConfig[$el.name] = userConfig[$el.name] = $el.value
     }
-    document.body.classList.toggle('home', !config.alwaysUseLatestTweets)
-    chrome.storage.local.set(config)
+    document.body.classList.toggle('home', !optionsConfig.alwaysUseLatestTweets)
+    chrome.storage.local.set(userConfig)
   })
 })
