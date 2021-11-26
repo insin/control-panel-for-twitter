237fdc572b0898e0d41a97908ffec0f20d045d29
Jonny Buchanan <jonathan.buchanan@gmail.com>
2021-11-26 20:48:31 +1000

    Release v2.14.1
    ## Fixed
    
    - Fixed dismissing the menu after the custom "Mute this conversation" menu item is used to mute QTs of a particular tweet

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     51
+// @version     52
 // ==/UserScript==
 
 let debug = false
