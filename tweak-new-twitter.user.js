c29573688e118c62e64e6064e389f83fd6255088
Jonny Buchanan <jonathan.buchanan@gmail.com>
2021-12-06 09:23:52 +1000

    Release v2.14.2
    - Added support for hiding random tweets from any of your lists on the algorithmic ("Home") timeline

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     52
+// @version     53
 // ==/UserScript==
 
 let debug = false
