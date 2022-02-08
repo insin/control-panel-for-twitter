f1a2c263a0da67e00ddb709063f8395837d5f4f3
Jonny Buchanan <jonathan.buchanan@gmail.com>
2022-02-08 14:40:52 +1000

    Release v2.14.4
    - Changed the implementation of hiding "More Tweets", as a Twitter change broke the previous implementation

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     54
+// @version     55
 // ==/UserScript==
 
 let debug = false
