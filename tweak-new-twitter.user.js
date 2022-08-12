01a7c8c4d84bac48b91eac55742e28cc4eb582ed
Jonny Buchanan <jonathan.buchanan@gmail.com>
2022-08-12 14:57:31 +1000

    Fix userscript @version

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     57
+// @version     58
 // ==/UserScript==
 
 let debug = false
