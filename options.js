c62293bcf56b32869505a560e923cc4cbdc3c9b9
Jonny Buchanan <jonathan.buchanan@gmail.com>
2022-08-19 14:34:36 +1000

    Release v2.15.3
    More mobile fixes after recent Twitter changes
    
    - Fixed hiding the mobile "App is best!" nag on the login screen
    - Fixed hiding the "Open app" nag on mobile when viewing a tweet
    - Fixed hiding a user's total tweet count on mobile when hiding metrics
    - Fixed skipping the Explore page on mobile when using the back button on the Search page while hiding Explore page contents

--- options.js
+++ options.js
@@ -142,7 +142,7 @@ function exportConfig() {
   let $a = document.createElement('a')
   $a.download = 'tweak-new-twitter.config.txt'
   $a.href = URL.createObjectURL(new Blob([
-    JSON.stringify({version: '2.15.2', ...sortProperties(optionsConfig)}, null, 2)
+    JSON.stringify({version: '2.15.3', ...sortProperties(optionsConfig)}, null, 2)
   ], {type: 'text/plain'}))
   $a.click()
   URL.revokeObjectURL($a.href)
