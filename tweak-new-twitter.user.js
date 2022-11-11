b7300dfc4bc5ab5b33b40b017b1cfac7d3cb4048
Jonny Buchanan <jonathan.buchanan@gmail.com>
2022-11-11 17:30:25 +1000

    Release v2.16.0
    - Dim (default) or hide Twitter Blue checkmarks on the timeline, profiles and tweet threads
    - Hide the "Verified" tab on the Notifications page
    - Changed where the main script runs so it can access React props, hopefully shouldn't have any impact on functionality

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     63
+// @version     64
 // ==/UserScript==
 
 let debug = false
