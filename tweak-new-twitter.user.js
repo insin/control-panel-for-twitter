2d25dfd20251c9a39dc2ae8d464610f8dd912afb
Jonny Buchanan <jonathan.buchanan@gmail.com>
2022-11-12 15:22:11 +1000

    Release v2.16.1
    - Restyled the main timline switcher controls as timeline tabs
    - Started hiding… more stuff in profile and individual tweet timelines

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     65
+// @version     66
 // ==/UserScript==
 
 let debug = false
