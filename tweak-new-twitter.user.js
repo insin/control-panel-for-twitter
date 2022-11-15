8f72a35e633f74572fddab5631d8e357d8551a77
Jonny Buchanan <jonathan.buchanan@gmail.com>
2022-11-16 03:25:49 +1000

    Release v2.16.4
    - Twitter Blue checks are now replaced with a Twitter Blue logo instead of being dimmed
    - The check in the verified account type popup on user profiles is now handled
    - Fix hiding total tweet metrics in desktop user profile headings

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     68
+// @version     69
 // ==/UserScript==
 
 let debug = false
