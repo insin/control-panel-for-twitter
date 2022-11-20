48da233ad3b5d8dab4a64657479ef81e8a061701
Jonny Buchanan <jonathan.buchanan@gmail.com>
2022-11-21 01:08:08 +1000

    Release v2.18.1
    - Fixed retweeted quote tweets not being hidden/separated based on Quote Tweets config
    - Dropped "Options" from the toolbar button title

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -5,7 +5,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     71
+// @version     72
 // ==/UserScript==
 
 let debug = false
