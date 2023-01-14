62f3e148948ef30e5bb7fe1ad078aeaa2de6f785
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-01-15 02:18:45 +1000

    Release v2.21.0
    - Added support for the new tabbed timeline

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -5,7 +5,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     79
+// @version     80
 // ==/UserScript==
 
 let debug = false
