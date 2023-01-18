27659c6e68877c6ee77ad4c6275ac3f28f9c969d
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-01-18 22:10:39 +1000

    Release v2.21.3
    - Fixed re-clicking the Following tab when it's already active on the Home timeline

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -5,7 +5,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     82
+// @version     83
 // ==/UserScript==
 
 let debug = false
