df0814a18a677b8f0d5fbdd4da61692f680032b4
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-01-19 23:54:32 +1000

    Release v2.21.5
    - Fixed the Retweets tab not being styled as active when selected when "For you" is not being hidden

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -5,7 +5,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     84
+// @version     85
 // ==/UserScript==
 
 let debug = false
