0a1303da624d49c582b85e10734daa55f9410a32
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-01-20 00:37:08 +1000

    Release v2.21.6
    Menu fixes after a Twitter markup change
    
    - Fixed hover style on custom menu items
    - Fixed closing the menu after "Mute this conversation" is used to mute a specific QT

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -5,7 +5,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     85
+// @version     86
 // ==/UserScript==
 
 let debug = false
