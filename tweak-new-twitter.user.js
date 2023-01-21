65027f0549593353471515eec37b885e78e96f7a
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-01-21 23:16:09 +1000

    Release v2.22.1
    - Fixed Tweak New Twitter not working if you open Twitter in a background tab then switch to it some time later [#198]

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -5,7 +5,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     88
+// @version     89
 // ==/UserScript==
 void function() {
 
