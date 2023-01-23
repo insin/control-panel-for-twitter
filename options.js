affbe2ebe057b251d3dd698531f393d356a827cd
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-01-24 04:52:50 +1000

    Release v2.23.2

--- options.js
+++ options.js
@@ -211,7 +211,7 @@ let $mutedQuotesLabel = /** @type {HTMLElement} */ (document.querySelector('#mut
 //#region Utility functions
 function exportConfig() {
   let $a = document.createElement('a')
-  $a.download = 'tweak-new-twitter-v2.23.1.config.txt'
+  $a.download = 'tweak-new-twitter-v2.23.2.config.txt'
   $a.href = URL.createObjectURL(new Blob([
     JSON.stringify(optionsConfig, null, 2)
   ], {type: 'text/plain'}))
