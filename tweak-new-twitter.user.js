89980450fdf9ce7b65dacf1ed386ad0c0080737b
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-01-25 17:04:20 +1000

    Release v2.24.0
    - Added Spanish translation by @rogama25
    - Added support for observing the tweet timeline in desktop media modals, so other features will work there, e.g. replacing Twitter Blue checks
    - Fixed hiding Views on the main tweet in desktop media modals
    - Fixed hiding the Like button instead of Views when the Buffer extension is also being used

--- tweak-new-twitter.user.js
+++ tweak-new-twitter.user.js
@@ -5,7 +5,7 @@
 // @namespace   https://github.com/insin/tweak-new-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     90
+// @version     91
 // ==/UserScript==
 void function() {
 
