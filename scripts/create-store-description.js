603394efa8974ac30642172627a68a2df5736876
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-01-26 13:04:59 +1000

    Add HTML output to the create-store-description script, for Firefox Add-ons

--- scripts/create-store-description.js
+++ scripts/create-store-description.js
@@ -1,12 +1,15 @@
-// Usage: npm run create-store-description -- ja
+// Usage:
+//   npm run create-store-description ja
+//   npm run create-store-description ja html
 const fs = require('fs')
 
 let localeCode = process.argv[2]
 let locale = JSON.parse(fs.readFileSync(`./_locales/${localeCode}/messages.json`, {encoding: 'utf8'}))
 let messages = Object.fromEntries(Object.entries(locale).map(([prop, value]) => ([prop, value.message])))
 
+let storeDescription
 with (messages) {
-  console.log(`
+  storeDescription = `
 ${reduceAlgorithmicContentOptionsLabel}
 
 – ${alwaysUseLatestTweetsLabel}
@@ -58,5 +61,24 @@ ${experimentsOptionsLabel}
   – ${verifiedAccountsInfo}
 – ${disableHomeTimelineLabel}
   – ${disableHomeTimelineInfo}
-  `)
+  `
 }
+
+if (process.argv[3] == 'html') {
+  // XXX This depends _very specifically_ on the way dashes, spaces and newlines
+  //     are used in the template string above.
+  storeDescription = storeDescription.trim()
+    // 2 nested items
+    .replace(/^– ([^\n]+)\n  – ([^\n]+)\n  – ([^\n]+)/gm, '<li>$1<ul>\n<li>$2</li>\n<li>$3</li></ul></li>')
+    // 1 nested item
+    .replace(/^– ([^\n]+)\n  – ([^\n]+)/gm, '<li>$1<ul>\n<li>$2</li></ul></li>')
+    // No nested items
+    .replace(/^– ([^\n]+)/gm, '<li>$1</li>')
+    // Section titles
+    .replace(/^([^\n<][^\n]+)\n\n/gm, '<strong>$1</strong>\n<ul>\n')
+    // Remaining empty lines
+    .replace(/^$/gm, '</ul>\n')
+    .replace(/$/, '\n</ul>')
+}
+
+console.log(storeDescription)
