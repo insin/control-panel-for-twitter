8cad79b9d26872f342d66df4b4045dd51b5d2fba
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-02-04 12:02:41 +1000

    Renamed the Safari version to "Control Panel for Twitter" because "Tweak New Twitter" was rejected from the app store
    It is what it is

--- safari/Shared (App)/Resources/Script.js
+++ safari/Shared (App)/Resources/Script.js
@@ -2,9 +2,9 @@ function show(platform, enabled, useSettingsInsteadOfPreferences) {
     document.body.classList.add(`platform-${platform}`);
 
     if (useSettingsInsteadOfPreferences) {
-        document.getElementsByClassName('platform-mac state-on')[0].innerText = "Tweak New Twitter’s extension is currently on. You can turn it off in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('platform-mac state-off')[0].innerText = "Tweak New Twitter’s extension is currently off. You can turn it on in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('platform-mac state-unknown')[0].innerText = "You can turn on Tweak New Twitter’s extension in the Extensions section of Safari Settings.";
+        document.getElementsByClassName('platform-mac state-on')[0].innerText = "Control Panel for Twitter’s extension is currently on. You can turn it off in the Extensions section of Safari Settings.";
+        document.getElementsByClassName('platform-mac state-off')[0].innerText = "Control Panel for Twitter’s extension is currently off. You can turn it on in the Extensions section of Safari Settings.";
+        document.getElementsByClassName('platform-mac state-unknown')[0].innerText = "You can turn on Control Panel for Twitter’s extension in the Extensions section of Safari Settings.";
         document.getElementsByClassName('platform-mac open-preferences')[0].innerText = "Quit and Open Safari Settings…";
     }
 
