de9b13e683bd57e2e23eb1ada4cbae86edffb5cb
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-02-07 14:35:31 +1000

    Release v3.2.0
    Renamed to Control Panel for Twitter

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // @namespace   https://github.com/insin/control-panel-for-twitter/
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
-// @version     95
+// @version     96
 // ==/UserScript==
 void function() {
 
