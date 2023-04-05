1502512e74273d6fa5b9491f350589178bc308b2
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-04-05 13:24:36 +1000

    Release v3.3.4
    - Replace the doge logo with the Twitter logo in Safari

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     100
+// @version     101
 // ==/UserScript==
 void function() {
 
