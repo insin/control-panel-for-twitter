e3b9c8f57b667032b1a45f5a6b6f9bd804d23acc
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-05-06 20:31:38 +1000

    Release v3.5.0
    - Added an option to hide the floating "See new Tweets" button in the main timeline
    - Added an option to hide the new "Connect" item in the desktop "More" menu
    - Updated options for hiding navigation and menu items to reflect recent Twitter changes
    - Fixed profile page detection the URL username has a different case from the user's canonical username

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     106
+// @version     107
 // ==/UserScript==
 void function() {
 
