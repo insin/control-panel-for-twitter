47f3213362a98b72e8f482a52f4d6b2b79bc6973
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-07-09 22:59:06 +1000

    Update the create-store-description script
    - the store description is now placed on the clipboard
    - fixed output issues which required manual editing after pasting

---
+++ scripts/create-store-description.mjs
@@ -0,0 +1,98 @@
+// Usage:
+//   npm run create-store-description ja
+//   npm run create-store-description ja html
+import fs from 'node:fs'
+
+import clipboard from 'clipboardy'
+
+let localeCode = process.argv[2]
+let locale = JSON.parse(fs.readFileSync(`./_locales/${localeCode}/messages.json`, {encoding: 'utf8'}))
+let messages = Object.fromEntries(Object.entries(locale).map(([prop, value]) => ([prop, value.message])))
+
+let storeDescription = `
+${messages.homeTimelineOptionsLabel}
+
+• ${messages.alwaysUseLatestTweetsLabel}
+  • ${messages.hideForYouTimelineLabel}
+• ${messages.retweetsLabel}
+  • ${messages.option_separate}
+• ${messages.hideUnavailableQuoteTweetsLabel}
+• ${messages.mutableQuoteTweetsLabel}
+  • ${messages.mutableQuoteTweetsInfo}
+• ${messages.hideSeeNewTweetsLabel}
+• ${messages.hideWhoToFollowEtcLabel}
+• ${messages.fullWidthContentLabel}
+  • ${messages.fullWidthContentInfo}
+
+${messages.uiImprovementsOptionsLabel}
+
+• ${messages.hideViewsLabel}
+• ${messages.hideVerifiedNotificationsTabLabel}
+• ${messages.twitterBlueChecksLabel}
+  • ${messages.twitterBlueChecksOption_replace}
+• ${messages.hideTwitterBlueRepliesLabel}
+• ${messages.hideTwitterBlueUpsellsLabel}
+• ${messages.hideSubscriptionsLabel}
+• ${messages.addAddMutedWordMenuItemLabel_desktop}
+• ${messages.fastBlockLabel}
+• ${messages.hideProfileRetweetsLabel}
+• ${messages.defaultToLatestSearchLabel}
+• ${messages.tweakQuoteTweetsPageLabel}
+
+${messages.uiTweaksOptionsLabel}
+
+• ${messages.dontUseChirpFontLabel}
+• ${messages.disableTweetTextFormattingLabel}
+• ${messages.navBaseFontSizeLabel}
+• ${messages.navDensityLabel}
+  • ${messages.option_comfortable} / ${messages.option_compact}
+• ${messages.dropdownMenuFontWeightLabel}
+• ${messages.uninvertFollowButtonsLabel}
+  • ${messages.followButtonStyleOption_monochrome} / ${messages.followButtonStyleOption_themed}
+
+${messages.reduceAlgorithmicContentOptionsLabel}
+
+• ${messages.hideSidebarContentLabel}
+• ${messages.hideExplorePageContentsLabel}
+• ${messages.hideMoreTweetsLabel}
+
+${messages.reduceEngagementOptionsLabel}
+
+• ${messages.hideMetricsLabel}
+• ${messages.reducedInteractionModeLabel}
+  • ${messages.reducedInteractionModeInfo}
+• ${messages.disableHomeTimelineLabel}
+  • ${messages.disableHomeTimelineInfo}
+
+${messages.hideUnusedUiItemsOptionsLabel}
+
+• ${messages.hideBookmarkButtonLabel}
+• ${messages.hideShareTweetButtonLabel}
+• ${messages.hideAccountSwitcherLabel}
+• ${messages.hideMessagesDrawerLabel}
+• ${messages.hideMoreSlideOutMenuItemsOptionsLabel_desktop}
+`.trim()
+
+if (process.argv[3] == 'html') {
+  // XXX This depends _very specifically_ on the way dashes, spaces and newlines
+  //     are used in the template string above.
+  storeDescription = `<strong>${messages.features}:</strong>\n\n` + storeDescription
+    // 2 nested items
+    .replace(/^• ([^\n]+)\n  • ([^\n]+)\n  • ([^\n]+)/gm, '<li>$1<ul>\n<li>$2</li>\n<li>$3</li></ul></li>')
+    // 1 nested item
+    .replace(/^• ([^\n]+)\n  • ([^\n]+)/gm, '<li>$1<ul>\n<li>$2</li></ul></li>')
+    // No nested items
+    .replace(/^• ([^\n]+)/gm, '<li>$1</li>')
+    // Section titles
+    .replace(/^([^\n<][^\n]+)\n\n/gm, '<strong>$1</strong>\n<ul>\n')
+    // Remaining empty lines
+    .replace(/^$/gm, '</ul>\n')
+    .replace(/$/, '\n</ul>')
+} else {
+  storeDescription = `${messages.features}:\n\n` + storeDescription
+}
+
+storeDescription += '\n\nTWITTER, TWEET and RETWEET are trademarks of Twitter Inc. or its affiliates'
+
+clipboard.writeSync(storeDescription)
+console.log(storeDescription)
