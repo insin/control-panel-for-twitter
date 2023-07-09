8953c82736d80557dc416bcd81973676be28ba7a
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-07-09 22:13:36 +1000

    Release v3.11.0
    - Added an option to hide Retweets in user profiles
    - Added an option to change navigation bar density on desktop (Default, Comfortable or Compact)
    - Added an option to disable bold and italic text formatting in tweets
    - Moved formatting-related options to the "UI tweaks" section
    - Fixed getting the username when muting specific Quote Tweets
    - Removed Twitter Circle functionality

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     114
+// @version     115
 // ==/UserScript==
 void function() {
 
