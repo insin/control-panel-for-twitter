b490705ab1b0206a1362dd81f9460173002731f5
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-07-11 22:40:08 +1000

    Release v3.11.1
    - Fixed detection of user profile pages which have certain special characters in their display name
    - Fixed hiding "Show when sidebar is hidden" option when the "Hide Explore" option isn't enabled

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     115
+// @version     116
 // ==/UserScript==
 void function() {
 
