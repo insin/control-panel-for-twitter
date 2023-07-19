1945951e6f495a07de778dd246811e73eaf636f7
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-07-20 02:11:29 +1000

    Release v3.11.3
    - Fixed profile page detection when viewing the Subs or Highlights tab
    - Fixed hiding the Subs tab when the profile also has a Highlights tab
    - Fixed conflicts with the Twitter Media Assist extension

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     117
+// @version     118
 // ==/UserScript==
 void function() {
 
