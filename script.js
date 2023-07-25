cf29709d7c78d633c7953bca86b2c9675e6376ee
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-07-26 00:45:52 +1000

    Release v3.12.2
    - Added support for "Liked/Retweeted by" user timelines - Blue checks are now processed in them
    - Fixed handling of desktop media modal tweet timelines

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     120
+// @version     121
 // ==/UserScript==
 void function() {
 
