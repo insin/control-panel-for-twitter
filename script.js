4a80f14f69d5aac49d3ffea26fcd0e688560ed9b
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-07-26 08:00:57 +1000

    Release v3.13.0
    - Fixed hiding the X logo - Twitter changed it
    - Replace the logo by default

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     121
+// @version     122
 // ==/UserScript==
 void function() {
 
