819ba8d3ed2f44df3a8eea86f8bb9b31ed901bb0
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-07-26 11:42:58 +1000

    Release v3.13.1
    - Reduce/remove the brief flash of the X logo on initial load and refresh
    - Use the last theme colour we saw for the loading logo, defaulting to blue

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     122
+// @version     123
 // ==/UserScript==
 void function() {
 
