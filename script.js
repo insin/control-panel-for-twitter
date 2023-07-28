9eafb8303dfb04e004094c8ff078045f08977d71
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-07-28 22:50:50 +1000

    Release v3.13.3
    - Fixed the "X" title being displayed before the app has loaded

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     124
+// @version     125
 // ==/UserScript==
 void function() {
 
