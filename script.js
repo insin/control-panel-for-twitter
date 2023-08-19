401a43e3fabd2c1500eb595c943ebb046716c28b
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-08-19 18:47:41 +1000

    Release v3.15.2
    - Fixed replacing "Post your reply!" on mobile

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     129
+// @version     130
 // ==/UserScript==
 void function() {
 
