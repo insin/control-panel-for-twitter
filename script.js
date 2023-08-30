392a652b274cc3d9f3cc791be3340f66347a3bf9
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-08-31 09:41:32 +1000

    Release v3.15.4
    - Fixed all timeline functionality after Twitter reverted a timeline change they made in June
    - Fixed hiding the Verified Orgs nav item

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     131
+// @version     132
 // ==/UserScript==
 void function() {
 
