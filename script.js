7209634abfedbbdb10a013cf77d165abaf9f97e7
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-09-06 20:44:30 +1000

    Release v3.15.5
    - Fixed desktop modal timeline handling

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     132
+// @version     133
 // ==/UserScript==
 void function() {
 
