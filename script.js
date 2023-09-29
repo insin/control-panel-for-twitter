f55962c1961cd390f316c33f058a06680ffbdcbd
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-09-30 03:55:48 +1000

    Release v3.19.0
    - Added an option under "Hide Twitter Blue replies" to show replies from "verified" business accounts
    - Changed "Hide Twitter Blue replies" to be more aggressive by default
      - Replies from accounts with over 1 million followers are no longer shown by default
      - Replies from "Verified" business accounts are no longer shown by default
    - Changed formatting of Quote Tweet counts to only use the compact format for 10,000 and above
    - Fixed restored Quote Tweets/Retweets/Likes links not being added to certain tweets

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     136
+// @version     137
 // ==/UserScript==
 void function() {
 
