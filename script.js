b88f5b916b8b443fa10d7231e3ca441f7c9636a0
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-10-05 20:12:15 +1000

    Release v3.20.0
    - Added an option to restore headlines under external links with large media cards

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     137
+// @version     138
 // ==/UserScript==
 void function() {
 
