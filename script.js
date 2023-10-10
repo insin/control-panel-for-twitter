0479a23f5ffdd1b682735713bc5390bc3cbb24e2
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-10-11 02:55:43 +1000

    Release v3.20.1
    - Fixed link headlines not being restored in Notifications

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     138
+// @version     139
 // ==/UserScript==
 void function() {
 
