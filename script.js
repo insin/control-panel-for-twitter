c9b6206afb192b4833a64771093041a91db26710
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-10-26 14:40:22 +1000

    Release v3.21.3
    - Removed the 'Hide the "Home" heading' feature, as Twitter have removed the Home heading from the desktop version
    - Fixed adding "Add muted word" to the "More" menu on desktop

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     141
+// @version     142
 // ==/UserScript==
 void function() {
 
