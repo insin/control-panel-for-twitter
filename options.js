eb28c0ec5cdf9934bd50acbf6491323e0d213c0e
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-10-29 19:03:58 +1000

    Release v3.21.4
    - Hiding the Bookmark button under tweets now also hides the new button under every timeline tweet
    - Fixed the media button in the desktop timeline Tweet box being hidden when hiding the "For you" timeline
    - Fixed staying on the Retweets/Shared Tweets timeline when the new Timeline settings modal is opened

--- options.js
+++ options.js
@@ -247,7 +247,7 @@ let $mutedQuotesLabel = /** @type {HTMLElement} */ (document.querySelector('#mut
 //#region Utility functions
 function exportConfig() {
   let $a = document.createElement('a')
-  $a.download = 'control-panel-for-twitter-v3.21.3.config.txt'
+  $a.download = 'control-panel-for-twitter-v3.21.4.config.txt'
   $a.href = URL.createObjectURL(new Blob([
     JSON.stringify(optionsConfig, null, 2)
   ], {type: 'text/plain'}))
--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     142
+// @version     143
 // ==/UserScript==
 void function() {
 
