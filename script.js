de3449ba52863267fbc65c2374202dd388574eaf
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-12-13 11:35:03 +1000

    Release v3.21.5
    - Fixed disabling the Chirp font
    - Fixed font in restored headlines, Quote Tweets link etc.
    - Fixed the Twitter logo sometimes being purple
    - Fixed the initial page not being handled when opened in the background
    - Fixed the top-level Settings page not being handled on desktop when the sidebar is collapsed

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     143
+// @version     144
 // ==/UserScript==
 void function() {
 
