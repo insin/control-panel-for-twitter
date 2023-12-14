4530c7541dbbb63245dc18ef16e09d38d1c6dce1
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-12-14 12:10:52 +1000

    Release v3.22.0
    - Added an option to remove Grok nav items/upsells (enabled by default)
    - Fixed a single item appearing in the profile page sidebar when hiding sidebar content on desktop - removed special handling for the now-removed sidebar media box

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     144
+// @version     145
 // ==/UserScript==
 void function() {
 
