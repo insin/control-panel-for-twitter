da3af51c6bf31a8e09db6380e77975180b5f4a11
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-12-23 01:38:04 +1000

    Release v3.22.1
    - Fixed Views being displayed under Tweets at some wider mobile resolutions after a Twitter change
    - Fixed "Hide Views under tweets" hiding the bookmark button when viewing media on desktop

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     145
+// @version     146
 // ==/UserScript==
 void function() {
 
