892efba687b0ffd551cc91ef2265ab33bd5ab724
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-01-06 14:25:13 +1000

    Release v3.22.2
    - When restoring headlines, hide the "From <domain>" link Twitter added after link cards

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     146
+// @version     147
 // ==/UserScript==
 void function() {
 
