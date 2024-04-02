7e9d3b96c79753b80483b189ed3262ba11199215
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-04-02 15:06:35 +1000

    Release v3.23.2
    - Added an option to hide all inline prompts in the timeline (enabled by default)
    - Fixed the "Post" button not being replaced with "Tweet" in the Tweet editor
    - Changed the selectors for hiding Premium/Verified nav items to be less specific, as these URLs keep changing

--- options.js
+++ options.js
@@ -243,7 +243,7 @@ let $mutedQuotesLabel = /** @type {HTMLElement} */ (document.querySelector('#mut
 //#region Utility functions
 function exportConfig() {
   let $a = document.createElement('a')
-  $a.download = 'control-panel-for-twitter-v3.23.1.config.txt'
+  $a.download = 'control-panel-for-twitter-v3.23.2.config.txt'
   $a.href = URL.createObjectURL(new Blob([
     JSON.stringify(optionsConfig, null, 2)
   ], {type: 'text/plain'}))
--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @match       https://twitter.com/*
 // @match       https://mobile.twitter.com/*
 // @run-at      document-start
-// @version     149
+// @version     150
 // ==/UserScript==
 void function() {
 
