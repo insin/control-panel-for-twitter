59219a3d15eb5b8e1c821d231932ba192169a2f4
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-04-04 18:12:23 +1000

    Added a build script to create an MV3 version for Chrome-based browsers
    Closes #529

---
+++ scripts/build.js
@@ -0,0 +1,23 @@
+const {execSync} = require('child_process')
+const path = require('path')
+const fs = require('fs')
+
+let manifestVersions = [2, 3]
+if (process.argv[2] && manifestVersions.includes(Number(process.argv[2]))) {
+  manifestVersions = [Number(process.argv[2])]
+}
+
+for (let manifestVersion of manifestVersions) {
+  console.log(`\nBuilding MV${manifestVersion} version`)
+  let manifestFile = `manifest.mv${manifestVersion}.json`
+  let manifestData = require(`../${manifestFile}`)
+  fs.copyFileSync(`./${manifestFile}`, './manifest.json')
+  execSync(`node_modules/.bin/web-ext${process.platform == 'win32' ? '.cmd' : ''} build`, {stdio: 'inherit'})
+  let renameTo = `./web-ext-artifacts/control_panel_for_twitter-${manifestData['version']}.mv${manifestVersion}.zip`
+  fs.renameSync(
+    `./web-ext-artifacts/control_panel_for_twitter-${manifestData['version']}.zip`,
+    renameTo,
+  )
+  console.log('Moved to:', path.resolve(renameTo))
+  fs.rmSync('./manifest.json')
+}
---
+++ scripts/copy.js
@@ -0,0 +1,2 @@
+const fs = require('fs')
+fs.copyFileSync(process.argv[2], process.argv[3])
