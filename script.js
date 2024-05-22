722da3d5e2735fc3e7aade9dc2d1cc646aa93c90
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-05-22 18:25:28 +1000

    Release v4.1.1
    - Fixed switching between tabs on the Home timeline, after Twitter changed the timeline structure
    - Fixed redirecting away from the Home timeline when it's disabled

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     155
+// @version     156
 // ==/UserScript==
 void function() {
 
