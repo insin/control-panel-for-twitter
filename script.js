5187a9d7584e5a5977dcf983eb36e3bcb753477f
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-08-02 08:56:12 +1000

    Release v4.1.3
    - Hiding upsells now hides the new "Get verified" button in your own profile

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     157
+// @version     158
 // ==/UserScript==
 void function() {
 
