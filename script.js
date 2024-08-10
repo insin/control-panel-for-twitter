9b2621720d9d18580b5bc4a14b62dcb10b2b64b1
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-08-10 21:12:18 +1000

    Release v4.2.0
    - Added an option to set your preferred default for sorting replies

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     158
+// @version     159
 // ==/UserScript==
 void function() {
 
