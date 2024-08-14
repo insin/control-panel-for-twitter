062625eeff4be8c6d44ee3f9656464c756e81a39
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-08-14 19:59:51 +1000

    Release v4.2.2
    - Hid a new Premium upsell in the mobile timeline header

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     160
+// @version     161
 // ==/UserScript==
 void function() {
 
