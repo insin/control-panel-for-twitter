5fb6d816981c2c89e1fba14e211038dc1507f643
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-08-16 15:41:37 +1000

    Release v4.2.5
    - Hide new Jobs nav item on mobile

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     163
+// @version     164
 // ==/UserScript==
 void function() {
 
