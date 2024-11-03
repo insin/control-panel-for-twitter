4648a5073bb02bd1142a52665e99a3e3de08ddf1
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-11-03 11:27:41 +1000

    Release v4.3.2
    - Hide new Analytics menu item, which is a Premium upsell

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     166
+// @version     167
 // ==/UserScript==
 void function() {
 
