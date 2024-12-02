8df8809229586f139d04ed49a09bae4f748c2967
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-12-02 22:21:15 +1000

    Release v4.4.0
    - The "Hide Grok" option now hides the new button which Twitter has added to all Tweets
    - Blanket hide all upsell links in your own profile - hides the current Premium upsell which will no doubt be replaced with another
    - Simplified hiding Views and bookmark buttons under Tweets, to avoid hiding the wrong button when Twitter changes things
    - Fixed detecting when we're on the Bookmarks page, as Twitter recently broke the page title
    - The Firefox version of Control Panel for Twitter now requires Firefox 121 or later

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     167
+// @version     168
 // ==/UserScript==
 void function() {
 
