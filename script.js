05f24c144200d9db2c14e1ad6c1c94778c6f47ca
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-12-05 20:13:37 +1000

    Release v4.4.1
    - Restore the theme color for Tweet buttons as part of the "Replace X branding changes" option

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     168
+// @version     169
 // ==/UserScript==
 void function() {
 
