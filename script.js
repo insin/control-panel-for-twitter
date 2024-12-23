d07462f618b240bfb748eada674557c8aa8cf9c0
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-12-23 10:18:34 +1000

    Release v4.5.0
    - Added Italian translation by @katullo11
    - Improved French translation by @luclu7
    - Hide the Grok "Profile Summary" button in user hovercards on desktop

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     174
+// @version     175
 // ==/UserScript==
 void function() {
 
