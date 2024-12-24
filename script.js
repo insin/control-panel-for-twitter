cad43000107d9edc9f80c17aa7ed4f0315d02719
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-12-25 04:06:56 +1000

    Release v4.5.1
    - Replace daruma version of X logo (Japan-only)

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     175
+// @version     176
 // ==/UserScript==
 void function() {
 
