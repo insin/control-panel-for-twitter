7321852a562d511f2dd48bc7a92396e17636d8e3
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-01-08 19:03:50 +1000

    Release v4.5.2
    - Hide suggested Grok prompts in Tweets
    - Changed how Tweets are hidden to also hide the new separator between them

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     176
+// @version     177
 // ==/UserScript==
 void function() {
 
