f4ced4f4510167cb1611b0840d9b164f33860457
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-01-12 22:38:48 +1000

    Release v4.5.3
    - Fixed a layout issue in the Home timeline when hiding the For you timeline, but not hiding Grok suggestions in Tweets

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     177
+// @version     178
 // ==/UserScript==
 void function() {
 
