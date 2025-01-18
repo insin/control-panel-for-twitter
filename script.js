048c13e2477484e4697b8bc11b1ca9ddc2e02e91
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-01-18 12:51:29 +1000

    Release v4.5.4
    - Hide the new Grok drawer on desktop
    - Hide the new "Radar" Premium upsell in Search on desktop

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     178
+// @version     179
 // ==/UserScript==
 void function() {
 
