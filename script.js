25acc3b765a1200e2343c4cafba3859b1719ed9b
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-01-20 10:17:22 +1000

    Release v4.5.5
    - Hide another new Grok button in user profiles

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     179
+// @version     180
 // ==/UserScript==
 void function() {
 
