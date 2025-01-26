c28a6e83e8974ef2d81899b0171568538c5073e5
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-01-27 03:39:24 +1000

    Release v4.6.0
    - Added an experimental "Tweak new layout" option for people who have the new Twitter layout on desktop to try
    - Added support for hiding items in the new layout's More dialog
    - Fixed styling of the separated Retweets/Quote Tweets tab with the new layout
    - Fixed styling of hidden tabs with the new layout
    - Added restoring the "What's happening?" placeholder in the desktop timeline Tweet box
    - Hide the Grok summary at the top of search results

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     180
+// @version     181
 // ==/UserScript==
 void function() {
 
