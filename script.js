83d3cd63fb2fa839bb62644b5628185a536e9b14
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-02-13 07:20:25 +1000

    Fixed sidebar contents not being hidden on the Home timeline due to a Twitter change (currently) only on this screen

--- script.js
+++ script.js
@@ -3630,6 +3630,7 @@ const configureCss = (() => {
           /* Restore the sidebar to its old width */
           ${Selectors.SIDEBAR},
           ${Selectors.SIDEBAR} > div > div,
+          body.HomeTimeline ${Selectors.SIDEBAR_WRAPPERS} > div > div:first-child,
           ${Selectors.SIDEBAR_WRAPPERS} > div:first-child {
             width: 350px !important;
           }
@@ -3822,6 +3823,9 @@ const configureCss = (() => {
         // Only show the first sidebar item by default
         // Re-show subsequent non-algorithmic sections on specific pages
         cssRules.push(`
+          body.HomeTimeline ${Selectors.SIDEBAR_WRAPPERS} > div > div:not(:first-of-type) {
+            display: none;
+          }
           ${Selectors.SIDEBAR_WRAPPERS} > div:not(:first-of-type) {
             display: none;
           }
