c307e3e5d26a2ae2330a4abc3ad98b379c26ba0e
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-02-13 07:21:48 +1000

    Release v4.7.1
    - Fixed sidebar contents not being hidden on the Home timeline due to a Twitter change (currently) only on this screen

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     184
+// @version     185
 // ==/UserScript==
 void function() {
 
