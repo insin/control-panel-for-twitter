95384a58efcc34fd9bae5747d23885a5da20ce2d
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-02-19 08:36:26 +1000

    Release v4.8.2
    - Added hiding "Discover new Lists" suggestions in the Lists page
    - Added missing translations for "What's happening?" and "Tweet your reply" where Twitter currently uses the English version
    - Changed the Hide "Discover more" tweets option to Hide "Discover" suggestions
    - Fixed most "What's happening?" translations based on Twitter's locale data
    - Fixed hiding a Premium upsell in the Communities header
    - Fixed search filters in the sidebar being hidden (again) - added a temporary workaround for Twitter leaving a stray divider at the top of the sidebar

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     187
+// @version     188
 // ==/UserScript==
 void function() {
 
