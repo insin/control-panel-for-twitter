97c5c0ea23b98e1d28225455d0d7945a2c018fee
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-03-13 18:08:00 +1000

    v4.10.0
    - Added an option to hide Grok Tweets
    - Fixed the restored Twitter reply box placeholder for Japanese and a few other languages
    - Fixed restoring Twitter placeholders in the desktop Compose Tweet modal after using Drafts/Media/GIF search/Schedule
    - Fixed the empty title on the Messages page on desktop causing bugs on the next page you visit (e.g. For you timeline re-appearing in Home)
    - Removed top-level toggles for some groups of options in the options page

--- options.js
+++ options.js
@@ -284,7 +284,7 @@ let $showBlueReplyFollowersCountLabel = /** @type {HTMLElement} */ (document.que
 //#region Utility functions
 function exportConfig() {
   let $a = document.createElement('a')
-  $a.download = 'control-panel-for-twitter-v4.9.0.config.txt'
+  $a.download = 'control-panel-for-twitter-v4.10.0.config.txt'
   $a.href = URL.createObjectURL(new Blob([
     JSON.stringify(optionsConfig, null, 2)
   ], {type: 'text/plain'}))
