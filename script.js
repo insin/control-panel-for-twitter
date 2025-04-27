2fc1fb0f28ec19900e63246d9c1c544bedcfaa9a
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-04-28 02:24:10 +1000

    Release v4.10.1
    - Hide the ad in What's happening when not hiding sidebar contents
    - Hide the ad at the top of Explore when not hiding Explore's contents
    - Hide a new "This profile is verified" Premium upsell when viewing a Premium profile
    - Hide a new Premium Analytics upsell in your own profile when not hiding sidebar contents
    - Hide a new hovercard Premium upsell on desktop
    - Fixed hiding the Premium upsell in the Home sidebar when not hiding sidebar contents
    - Fixed hiding the user's own replies if they have Premium, when hiding Premium replies in threads
    - Process blue checks in the sidebar user box when not hiding sidebar contents
    - Process blue checks in Explore when not hiding Explore's contents
    - Don't run on URLs used for OAuth authorization

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     190
+// @version     191
 // ==/UserScript==
 void function() {
 
