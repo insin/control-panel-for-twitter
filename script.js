52de4d1c19a8945aebdabd2eaea7be1b8c57c6ec
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-05-17 03:52:39 +1000

    Release v4.11.0
    - Added an option to hide the Live bar in the mobile timeline when someone you follow is broadcasting
    - Added an option to hide the Live with X box in the desktop sidebar
    - Added an option to hide the What's happening box in the desktop sidebar
    - Added an option to hide Who to follow and other suggested follow boxes in the desktop sidebar
    - Moved these to a new Sidebar options group, as they don't all belong in the Remove algorithmic content group
    - Added changing "Live on X" to "Live on Twitter" when Replace X branding changes is enabled
    - Added hiding promoted trends
    - Added hiding Install button cards in some Grok tweets
    - Added hiding Ask Grok button in Tweets
    - Added hiding a new premium upsell in the desktop sidebar
    - Fixed a link headline restoration false positive on Install button cards in Grok tweets
    - Fixed hiding the ad in What's happening when not hiding sidebar contents
    - Fixed Japanese translation wording and sentence endings
    - Increased timeout waiting for the "you aren't verified yet" premium upsell to appear in your own profile
    - Removed the Hide Pro nav option, as it's no longer applicable

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     191
+// @version     192
 // ==/UserScript==
 void function() {
 
