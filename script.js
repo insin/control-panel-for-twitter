d0e54a9d75a9267d3bff1a33ee6e0d3858721702
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-05-26 16:06:46 +1000

    Release v4.11.1
    - Hid another new Premium upsell in Premium user profiles

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     192
+// @version     193
 // ==/UserScript==
 void function() {
 
