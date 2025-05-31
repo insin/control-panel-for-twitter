dab3344b15fbb5eeed87391755075600827259f0
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-05-31 19:14:25 +1000

    Release v4.12.0
    - Added an "Enabled" option to allow extension functionality to be turned off from its own options - when disabled, the icon in the toolbar will have a ⏻ badge (Safari) or be greyed out (all other browsers)
    - Added an option to restore the Tweet source label in focused Tweets (enabled by default)
    - Added hiding the "Upgrade to Premium+ to write longer posts" Premium upsell
    - Changed the options for hiding Premium replies to all be "Show" options so they're easier to understand - if a Premium reply matches any of your "Show" options, it will be shown
    - Split the option for showing Premium replies from verified orgs into separate options for Business and Government accounts
    - Fixed the same Tweets being repeatedly re-checked in focused Tweet timelines when new Tweets are added - should improve performance in long Tweet threads
    - Fixed flash of initial X loading logo
    - Fixed verified Government accounts getting the Twitter Blue check
    - Fixed the options in the Number of followers dropdown not being localised
    - Fixed the desktop nav font size option applying to the notifications count

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     193
+// @version     194
 // ==/UserScript==
 void function() {
 
