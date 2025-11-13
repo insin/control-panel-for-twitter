6c04e87725231fff604ea8a37afcfa516fdb8c01
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-11-14 04:59:09 +1000

    Release v4.15.0
    - Added a new option to redirect the Chat nav item to Messages
    - Fixed the sidebar and popups not being observed after visiting the Chat screen
    - Fixed the previous page's observers not being disconnected when navigating to the Chat screen
    - Hide the Chat drawer when configured to hide the Messages drawer on desktop
    - Hide the Chat bottom nav item when configured to hide the Messages bottom nav item on mobile

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     200
+// @version     201
 // ==/UserScript==
 void function() {
 
