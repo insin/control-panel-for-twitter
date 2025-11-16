b9f2ff05f3cd30104a0fbf0650c61740c118663b
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-11-16 10:54:14 +1000

    Release v4.15.1
    - Fixed redirecting the Chat nav item to Messages
    - Fixed the back button in Message requests bringing you to Chat

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     201
+// @version     202
 // ==/UserScript==
 void function() {
 
