e8d0199db1215213a9ef6e67bebf203a82dae03a
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-11-17 17:52:20 +1000

    Release v4.15.2
    - Fixed editing images and other sub-modal navigation

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @match       https://x.com/*
 // @match       https://mobile.x.com/*
 // @run-at      document-start
-// @version     202
+// @version     203
 // ==/UserScript==
 void function() {
 
